83168a0e25b207c279aae514059de6666b302af5
Ben Makusha <btmakusha@gmail.com>
2016-10-07 01:05:20 -0400

    Second phase of signin ui build. Added custom code for button and UIView

---
+++ Diffusion/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Diffusion
+//
+//  Created by Baynham Makusha on 10/7/16.
+//  Copyright © 2016 Ben Makusha. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0/255.0
+
+let KEY_UID = "uid"
---
+++ Diffusion/CustomButton.swift
@@ -0,0 +1,30 @@
+//
+//  CustomButton.swift
+//  Diffusion
+//
+//  Created by Baynham Makusha on 10/7/16.
+//  Copyright © 2016 Ben Makusha. All rights reserved.
+//
+
+import UIKit
+
+class CustomButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        imageView?.contentMode = .scaleAspectFit
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        layer.cornerRadius = self.frame.width / 2
+        
+    }
+
+}
