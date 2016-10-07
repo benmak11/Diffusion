6dac7879e1772150b48b5ef4953e702139323e87
Ben Makusha <btmakusha@gmail.com>
2016-10-06 23:58:11 -0400

    First phase of UI signin VC

--- Diffusion/SignInVC.swift
+++ Diffusion/SignInVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SignInVC.swift
 //  Diffusion
 //
 //  Created by Baynham Makusha on 10/6/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SignInVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
