c017daaec3d17d1c49b9820b739b8b52cedf2973
Ben Makusha <btmakusha@gmail.com>
2016-10-18 22:53:00 -0400

    Second stage of messaging

---
+++ Diffusion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Diffusion
+//
+//  Created by Baynham Makusha on 10/18/16.
+//  Copyright © 2016 Ben Makusha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
