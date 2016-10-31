de7d825146e72fe6e5f4b71fb7a5c2e52792795a
Ben Makusha <btmakusha@gmail.com>
2016-10-30 20:49:10 -0400

    Added message bubble colors for incoming and outgoing messages

--- Diffusion/MessagesVC.swift
+++ Diffusion/MessagesVC.swift
@@ -19,14 +19,34 @@ class MessagesVC: JSQMessagesViewController {
     var incomingBubbleImageView: JSQMessagesBubbleImage!
     
     
-    var userIsTypingRef: FIRDatabaseReference!
-    fileprivate var localTyping = false
+    var userIsTypingRef: FIRDatabaseReference!          // 1
+    fileprivate var localTyping = false                 // 2
     var isTyping: Bool{
         get {
             return localTyping
         } set {
+            // 3
             localTyping = newValue
             userIsTypingRef.setValue(newValue)
         }
+    }
+    
+    fileprivate func observeTyping() {
+        //let typingIndicatorRef = rootRef.child("typingIndicator")
+        
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageDataForItemAt indexPath: IndexPath!) -> JSQMessageData! {
+        return messages[indexPath.item]
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    fileprivate func setupBubbles() {
+        let factory = JSQMessagesBubbleImageFactory()
+        outgoingBubbleImageView = factory?.outgoingMessagesBubbleImage(with: UIColor.jsq_messageBubbleBlue())
+        incomingBubbleImageView = factory?.incomingMessagesBubbleImage(with: UIColor.jsq_messageBubbleLightGray())
     }
 }
